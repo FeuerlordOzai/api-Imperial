4ad8e33a43585340e92e8d975cf2cf853cbca17b
Malek <malekbouazizi@gmail.com>
2022-06-22 16:07:41 +0200

    add StoneToKilogram

---
+++ src/main/java/com/example/imperial/Controller.java
@@ -0,0 +1,17 @@
+package com.example.imperial;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    Implementation i = new Implementation();
+
+    @RequestMapping("/stoneToKilogram")
+    public double stoneToKilogram(@RequestParam double st){
+        return i.stoneToKilogram(st);
+    }
+
+}
--- src/test/java/com/example/imperial/ImperialApplicationTests.java
+++ src/test/java/com/example/imperial/ImperialApplicationTests.java
@@ -3,11 +3,20 @@ package com.example.imperial;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ImperialApplicationTests {
-
+    //sut = system under test
+    Implementation sut = new Implementation();
     @Test
-    void contextLoads() {
+    public void stoneToKilogram()
+    {
+        var result = sut.stoneToKilogram(1);
+
+        var expected = 6.35;
+
+        assertEquals(expected, result);
     }
 
 }
